af4b8ec3a6ffe133f081760eadb18b4f1264fb10
muzi-debugger <mmzolodev@gmail.com>
2025-01-24 19:22:32 +0200

    Retrieve Stripe secret key

--- ClickPay/settings.py
+++ ClickPay/settings.py
@@ -12,11 +12,13 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import environ
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env()
-environ.Env.read_env()
+environ.Env.read_env(env_file=os.path.join(BASE_DIR, ".env"))
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
